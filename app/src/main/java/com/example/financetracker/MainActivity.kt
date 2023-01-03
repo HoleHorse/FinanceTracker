50694112da64e5598416762a3a448bb1c9d9dc18
HoleHorse <tamirlankulzhanov@gmail.com>
2023-01-03 17:22:37 +0600

    add new nav

--- app/src/main/java/com/example/financetracker/MainActivity.kt
+++ app/src/main/java/com/example/financetracker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.financetracker
 
+import android.R.attr.button
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,10 +15,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myDataset = Datasource().getData()
+        val myDataset = Datasource().getRecent()
         val linearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         val recyclerView = findViewById<RecyclerView>(R.id.recent_list)
         recyclerView.adapter = ActionAdapter(this, myDataset, 7)
         recyclerView.layoutManager = linearLayoutManager
+        val add_new_btn: Button = findViewById(R.id.add_btn)
+        add_new_btn.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                setContentView(R.layout.add_new)
+            }
+        })
     }
 }
--- app/src/main/java/com/example/financetracker/data/Datasource.kt
+++ app/src/main/java/com/example/financetracker/data/Datasource.kt
@@ -4,7 +4,7 @@ import com.example.financetracker.R
 import com.example.financetracker.model.Action
 
 class Datasource {
-    fun getData(): List<Action> {
+    fun getRecent(): List<Action> {
         return listOf<Action>(
             Action("Food", 5000, R.drawable.arrow_down_circle_svgrepo_com),
             Action("Rent", 100000, R.drawable.arrow_down_circle_svgrepo_com),
