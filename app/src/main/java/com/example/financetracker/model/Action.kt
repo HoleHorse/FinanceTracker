57ac8466a1bd11720ca333ab3dec11291ba78c05
HoleHorse <tamirlankulzhanov@gmail.com>
2023-01-04 05:58:53 +0600

    lil change

--- app/src/main/java/com/example/financetracker/model/Action.kt
+++ app/src/main/java/com/example/financetracker/model/Action.kt
@@ -2,7 +2,7 @@ package com.example.financetracker.model
 
 import java.time.LocalDate
 
-data class Action (private var category: String, private var amount: Int, private var img: Int, private var date: LocalDate): java.io.Serializable {
+class Action (private var category: String, private var amount: Int, private var img: Int, private var date: LocalDate) {
 
     fun getCategory(): String {
         return this.category
